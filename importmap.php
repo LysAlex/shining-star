025ca21e70593f81631f4828b17777a025f7940c
Alex LYS <alex@lys-phorimavong.com>
2025-08-18 16:45:56 +0200

    First commit

--- importmap.php
+++ importmap.php
@@ -19,8 +19,25 @@ return [
     '@hotwired/stimulus' => [
         'version' => '3.2.2',
     ],
+    'tom-select/dist/css/tom-select.default.css' => [
+        'version' => '2.4.3',
+        'type' => 'css',
+    ],
     '@symfony/stimulus-bundle' => [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
+    ],
+    'tom-select' => [
+        'version' => '2.4.3',
+    ],
+    '@orchidjs/sifter' => [
+        'version' => '1.1.0',
+    ],
+    '@orchidjs/unicode-variants' => [
+        'version' => '1.1.2',
+    ],
+    'tom-select/dist/css/tom-select.default.min.css' => [
+        'version' => '2.4.3',
+        'type' => 'css',
     ],
     '@hotwired/turbo' => [
         'version' => '7.3.0',
