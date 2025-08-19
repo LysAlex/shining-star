868f26bcf5376fd0d6e84d63ad46d5b843469226
Alex LYS <alex@lys-phorimavong.com>
2025-08-19 15:49:24 +0200

    Test page

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,20 @@
+<?php
+// src/Controller/BaseController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BaseController extends AbstractController
+{
+    #[Route(path: '/', name: 'home')]
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('base.html.twig', [
+            'number' => $number??null
+        ]);
+    }
+}
